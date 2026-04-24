98e1ed81e5a67f38836e7864c67e44634fbe3140
Export <smg05892@gmail.com>
2026-04-24 15:16:37 +0300

    Initial Commit

---
+++ Car Rental/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Car Rental
+//
+//  Created by Michael Saakyan on 24.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Car RentalTests/Car_RentalTests.swift
@@ -0,0 +1,17 @@
+//
+//  Car_RentalTests.swift
+//  Car RentalTests
+//
+//  Created by Michael Saakyan on 24.04.2026.
+//
+
+import Testing
+@testable import Car_Rental
+
+struct Car_RentalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
